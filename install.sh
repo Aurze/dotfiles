b5ca3c088b006ab4c16a5d6b4bd41e127c83a4eb
Nicolas Arsenault <arsenault.nic@gmail.com>
2019-01-16 20:15:20 -0500

    creat a installation script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+install_package() {
+	pikaur -Sy $1
+}
+
+install_uninstall_package() {
+	package=$1
+	if pacman -Qs $package > /dev/null ; then
+		echo "$package is already install"
+	else
+		echo "installing $package"
+		install_package $package
+	fi
+}
+
+install_packages() {
+	packages=$(./$1/requirement.sh)
+	for package in $packages
+	do
+		install_uninstall_package $package
+	done
+	
+	stow -t ${HOME} -d $1 config
+
+	if [ -f ./$1/install.sh ]; then
+		./$1/install.sh
+	fi
+}
+
+install_packages $1
+
