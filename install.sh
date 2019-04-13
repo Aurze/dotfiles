4e1ae1b3bb6d167bbf08ca8c6a35555208d5b6b8
Nicolas Arsenault <arsenault.nic@gmail.com>
2019-04-13 14:12:58 -0400

    support for ubuntu device

--- install.sh
+++ install.sh
@@ -1,27 +1,56 @@
 #! /bin/bash
 
-install_package() {
-	pikaur -Sy $1
-}
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+echo "OS is $OS"
+echo "Version is $VER"
 
-install_uninstall_package() {
-	package=$1
-	if pacman -Qs $package > /dev/null ; then
-		echo "$package is already install"
-	else
-		echo "installing $package"
-		install_package $package
+install_package() {
+	packages=$@
+	if [ "$OS" == "Ubuntu" ]; then
+		echo "apt install $@"
+		sudo apt install $@
+	else	
+		echo "pikaur -Sy $@"
+		pikaur -Sy $@
 	fi
 }
 
 install_packages() {
 	packages=$(./$1/requirement.sh)
-	#for package in $packages
-	#do
-	#	install_uninstall_package $package
-	#done
+
+	if [ -f ./$1/preinstall.sh ]; then
+		./$1/preinstall.sh
+	fi
 	
-        pikaur -Sy $packages
+        install_package $packages
 
 	stow -t ${HOME} -d $1 config
 
---
+++ zsh/preinstall.sh
@@ -0,0 +1,3 @@
+wget http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-completions/xUbuntu_16.04/amd64/zsh-completions_0.30.0+1.1_amd64.deb zsh-completions.deb
+
+sudo dpkg -i zsh-completions.deb
